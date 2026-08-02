e46939dbbc85fb0cf41d4c62cc44f2f45102c06e
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-08-02 17:16:55 +0530

    feat: add solution for Rotate List (#61)

---
+++ 0061-rotate-list.py
@@ -0,0 +1,28 @@
+"""
+LeetCode #61 - Rotate List
+Difficulty: Medium
+"""
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def rotateRight(self, head, k):
+        if not head or not head.next:
+            return head
+        length = 1
+        tail = head
+        while tail.next:
+            tail = tail.next
+            length += 1
+        k %= length
+        if k == 0:
+            return head
+        new_tail = head
+        for _ in range(length - k - 1):
+            new_tail = new_tail.next
+        new_head = new_tail.next
+        new_tail.next = None
+        tail.next = head
+        return new_head
