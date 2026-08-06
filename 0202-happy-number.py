2429df697a7b16085d2b2c2097c505753033f4ae
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-08-06 23:04:36 +0530

    feat: add solution for Happy Number (#202)

---
+++ 0202-happy-number.py
@@ -0,0 +1,11 @@
+"""
+LeetCode #202 - Happy Number
+Difficulty: Easy
+"""
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen = set()
+        while n != 1 and n not in seen:
+            seen.add(n)
+            n = sum(int(c)**2 for c in str(n))
+        return n == 1
