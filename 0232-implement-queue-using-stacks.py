b72b151b7a4e06179595430cf050db9370e180ee
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-08-06 23:05:18 +0530

    feat: add solution for Implement Queue using Stacks (#232)

---
+++ 0232-implement-queue-using-stacks.py
@@ -0,0 +1,27 @@
+"""
+LeetCode #232 - Implement Queue using Stacks
+Difficulty: Easy
+"""
+class MyQueue:
+    def __init__(self):
+        self.stack_in = []
+        self.stack_out = []
+
+    def push(self, x):
+        self.stack_in.append(x)
+
+    def pop(self):
+        self._transfer()
+        return self.stack_out.pop()
+
+    def peek(self):
+        self._transfer()
+        return self.stack_out[-1]
+
+    def empty(self):
+        return not self.stack_in and not self.stack_out
+
+    def _transfer(self):
+        if not self.stack_out:
+            while self.stack_in:
+                self.stack_out.append(self.stack_in.pop())
