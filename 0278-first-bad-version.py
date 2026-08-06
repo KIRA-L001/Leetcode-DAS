35726f739b9fed81e0b54b8909f11288a945f93a
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-08-06 23:05:50 +0530

    feat: add solution for First Bad Version (#278)

---
+++ 0278-first-bad-version.py
@@ -0,0 +1,14 @@
+"""
+LeetCode #278 - First Bad Version
+Difficulty: Easy
+"""
+class Solution:
+    def firstBadVersion(self, n):
+        left, right = 1, n
+        while left < right:
+            mid = (left + right) // 2
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid + 1
+        return left
