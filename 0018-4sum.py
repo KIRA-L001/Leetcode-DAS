42ea38bfed7fba5afdbac627b3f877e4f9e73f32
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-08-07 10:37:40 +0530

    feat: add solution #18

---
+++ 0018-4sum.py
@@ -0,0 +1,34 @@
+
+"""
+LeetCode #18 - 4Sum
+Difficulty: Medium
+"""
+from typing import List
+
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        result = []
+        n = len(nums)
+        for i in range(n - 3):
+            if i and nums[i] == nums[i-1]:
+                continue
+            for j in range(i + 1, n - 2):
+                if j > i + 1 and nums[j] == nums[j-1]:
+                    continue
+                lo, hi = j + 1, n - 1
+                while lo < hi:
+                    s = nums[i] + nums[j] + nums[lo] + nums[hi]
+                    if s == target:
+                        result.append([nums[i], nums[j], nums[lo], nums[hi]])
+                        while lo < hi and nums[lo] == nums[lo+1]:
+                            lo += 1
+                        while lo < hi and nums[hi] == nums[hi-1]:
+                            hi -= 1
+                        lo += 1
+                        hi -= 1
+                    elif s < target:
+                        lo += 1
+                    else:
+                        hi -= 1
+        return result
