96e59db7dee706cf635275bc0b51a9bff37e3975
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-08-09 10:10:44 +0530

    feat: add solution for Trapping Rain Water (#42)

--- 0042-trapping-rain-water.py
+++ 0042-trapping-rain-water.py
@@ -6,19 +6,18 @@ from typing import List
 
 class Solution:
     def trap(self, height: List[int]) -> int:
-        left, right = 0, len(height) - 1
-        left_max = right_max = water = 0
-        while left < right:
-            if height[left] < height[right]:
-                if height[left] >= left_max:
-                    left_max = height[left]
+        lo, hi, left_max, right_max, water = 0, len(height) - 1, 0, 0, 0
+        while lo < hi:
+            if height[lo] < height[hi]:
+                if height[lo] >= left_max:
+                    left_max = height[lo]
                 else:
-                    water += left_max - height[left]
-                left += 1
+                    water += left_max - height[lo]
+                lo += 1
             else:
-                if height[right] >= right_max:
-                    right_max = height[right]
+                if height[hi] >= right_max:
+                    right_max = height[hi]
                 else:
-                    water += right_max - height[right]
-                right -= 1
+                    water += right_max - height[hi]
+                hi -= 1
         return water
