b8f82775d12a86e43f0b81a11c9e2dc9af3f8c95
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-08-09 10:12:07 +0530

    feat: add solution for Maximum Subarray (#53)

--- 0053-maximum-subarray.py
+++ 0053-maximum-subarray.py
@@ -6,8 +6,8 @@ from typing import List
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        max_sum = cur = nums[0]
-        for n in nums[1:]:
-            cur = max(n, cur + n)
-            max_sum = max(max_sum, cur)
-        return max_sum
+        best = cur = nums[0]
+        for num in nums[1:]:
+            cur = max(num, cur + num)
+            best = max(best, cur)
+        return best
