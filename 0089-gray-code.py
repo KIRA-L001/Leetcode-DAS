b906edcb72c2d48341a85d25b947ffc834c750c5
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-08-15 18:21:24 +0530

    feat: add solution #89

---
+++ 0089-gray-code.py
@@ -0,0 +1,13 @@
+"""
+LeetCode #89 - Gray Code
+Difficulty: Medium
+"""
+from typing import List
+
+class Solution:
+    def grayCode(self, n: int) -> List[int]:
+        result = [0]
+        for i in range(n):
+            for j in range(len(result) - 1, -1, -1):
+                result.append(result[j] | (1 << i))
+        return result
