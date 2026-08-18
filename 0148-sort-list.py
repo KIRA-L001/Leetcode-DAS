6b36efd9a1aae4a58f826e0103a5a0a03f5bc0de
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-08-18 12:12:02 +0530

    feat: add solution #148

---
+++ 0148-sort-list.py
@@ -0,0 +1,16 @@
+class ListNode:
+    def __init__(self, val=0, next=None): self.val = val; self.next = next
+class Solution:
+    def sortList(self, head):
+        if not head or not head.next: return head
+        slow = fast = head
+        while fast and fast.next: slow = slow.next; fast = fast.next.next
+        mid = slow.next; slow.next = None
+        l = self.sortList(head); r = self.sortList(mid)
+        dummy = ListNode(0); cur = dummy
+        while l and r:
+            if l.val < r.val: cur.next = l; l = l.next
+            else: cur.next = r; r = r.next
+            cur = cur.next
+        cur.next = l or r
+        return dummy.next
