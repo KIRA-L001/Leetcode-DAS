5b6ffe2218a5840aeced272455358c9d9be09c6f
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-08-18 12:12:08 +0530

    feat: add solution #257

---
+++ 0257-binary-tree-paths.py
@@ -0,0 +1,16 @@
+from typing import List, Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val; self.left = left; self.right = right
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        out = []
+        def dfs(n, path):
+            if not n: return
+            path += str(n.val)
+            if not n.left and not n.right: out.append(path)
+            else:
+                path += '->'
+                dfs(n.left, path); dfs(n.right, path)
+        dfs(root, '')
+        return out
