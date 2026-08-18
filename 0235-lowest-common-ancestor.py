01b65332b49f3379c204b1eb898b32ff96d2a839
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-08-18 12:12:13 +0530

    feat: add solution #235

---
+++ 0235-lowest-common-ancestor.py
@@ -0,0 +1,11 @@
+from typing import Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val; self.left = left; self.right = right
+class Solution:
+    def lowestCommonAncestor(self, root: Optional[TreeNode], p, q) -> Optional[TreeNode]:
+        while root:
+            if root.val < p.val and root.val < q.val: root = root.right
+            elif root.val > p.val and root.val > q.val: root = root.left
+            else: return root
+        return None
