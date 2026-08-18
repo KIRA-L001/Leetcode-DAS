ad19d99eec1c168769e8f16f4cc579790b45776d
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-08-18 12:10:41 +0530

    feat: add solution #208

---
+++ 0208-trie.py
@@ -0,0 +1,20 @@
+class Trie:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+    def insert(self, word: str) -> None:
+        node = self
+        for c in word: node = node.children.setdefault(c, Trie())
+        node.end = True
+    def search(self, word: str) -> bool:
+        node = self
+        for c in word:
+            if c not in node.children: return False
+            node = node.children[c]
+        return node.end
+    def startsWith(self, prefix: str) -> bool:
+        node = self
+        for c in prefix:
+            if c not in node.children: return False
+            node = node.children[c]
+        return True
