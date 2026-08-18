04863fb801dcb7bd1482174c6159b178f16969d1
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-08-18 12:14:03 +0530

    feat: add solution #260

---
+++ 0260-single-number-iii.py
@@ -0,0 +1,11 @@
+from typing import List
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        xor = 0
+        for n in nums: xor ^= n
+        mask = xor & -xor
+        a = b = 0
+        for n in nums:
+            if n & mask: a ^= n
+            else: b ^= n
+        return [a, b]
