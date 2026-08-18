0abb20375cdc3c717a68234c477299fb7555d9de
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-08-18 12:12:24 +0530

    feat: add solution #116

---
+++ 0116-populating-next-right.py
@@ -0,0 +1,16 @@
+from typing import Optional
+class Node:
+    def __init__(self, val=0, left=None, right=None, next=None):
+        self.val = val; self.left = left; self.right = right; self.next = next
+class Solution:
+    def connect(self, root: Optional[Node]) -> Optional[Node]:
+        if not root: return None
+        leftmost = root
+        while leftmost.left:
+            head = leftmost
+            while head:
+                head.left.next = head.right
+                if head.next: head.right.next = head.next.left
+                head = head.next
+            leftmost = leftmost.left
+        return root
