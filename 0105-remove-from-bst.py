4f9c123836ba77ea9f2fb9b3b4f6b7dad4ed1650
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-08-19 10:01:03 +0530

    feat: add solution #105

---
+++ 0105-remove-from-bst.py
@@ -0,0 +1,17 @@
+from typing import Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val; self.left = left; self.right = right
+class Solution:
+    def removeNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root: return None
+        if key < root.val: root.left = self.removeNode(root.left, key)
+        elif key > root.val: root.right = self.removeNode(root.right, key)
+        else:
+            if not root.left: return root.right
+            if not root.right: return root.left
+            m = root.right
+            while m.left: m = m.left
+            root.val = m.val
+            root.right = self.removeNode(root.right, m.val)
+        return root
