224b29af52e73970024ec25b403bb4772660b040
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-08-19 10:00:56 +0530

    feat: add solution #119

---
+++ 0119-pascals-triangle-ii.py
@@ -0,0 +1,7 @@
+from typing import List
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        row = [1]
+        for _ in range(rowIndex):
+            row = [1] + [row[i] + row[i+1] for i in range(len(row))] + [1]
+        return row
