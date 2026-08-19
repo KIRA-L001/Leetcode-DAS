68f6e9b49db525a8fc4a8cfde31a352bab8978d3
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-08-19 10:00:39 +0530

    feat: add solution #175

---
+++ 0175-combine-two-tables.py
@@ -0,0 +1,8 @@
+# SQL: SELECT p.firstName, p.lastName, a.city, a.state
+# FROM Person p LEFT JOIN Address a ON p.personId = a.personId
+class Solution:
+    def combineTwoTables(self, person: list, address: list) -> list:
+        m = {a["personId"]: a for a in address}
+        return [{"firstName": p["firstName"], "lastName": p["lastName"],
+                 "city": m.get(p["personId"], {}).get("city"),
+                 "state": m.get(p["personId"], {}).get("state")} for p in person]
