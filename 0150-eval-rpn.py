96068de515554d1fc606d15b3a2dfa2a2424df1c
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-08-19 10:00:42 +0530

    feat: add solution #150

---
+++ 0150-eval-rpn.py
@@ -0,0 +1,10 @@
+from typing import List
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        st = []
+        for t in tokens:
+            if t in '+-*/':
+                b, a = st.pop(), st.pop()
+                st.append(a+b if t=='+' else a-b if t=='-' else a*b if t=='*' else int(a/b))
+            else: st.append(int(t))
+        return st[0]
