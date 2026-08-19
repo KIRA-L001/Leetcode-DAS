9a9ae0707dc00e55e92c618d7b8009f21c6eeb7d
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-08-19 10:00:49 +0530

    feat: add solution #129

---
+++ 0129-sum-root-leaf.py
@@ -0,0 +1,15 @@
+from typing import Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val; self.left = left; self.right = right
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        total = 0
+        def dfs(n, cur):
+            nonlocal total
+            if not n: return
+            cur = cur * 10 + n.val
+            if not n.left and not n.right: total += cur
+            dfs(n.left, cur); dfs(n.right, cur)
+        dfs(root, 0)
+        return total
