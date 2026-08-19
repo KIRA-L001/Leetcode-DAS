8e747adc864fb6e8df83c0fc470c4305e390205f
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-08-19 10:00:34 +0530

    feat: add solution #143

---
+++ 0143-reorder-list.py
@@ -0,0 +1,13 @@
+class ListNode:
+    def __init__(self, val=0, next=None): self.val = val; self.next = next
+class Solution:
+    def reorderList(self, head) -> None:
+        if not head or not head.next: return
+        slow = fast = head
+        while fast and fast.next: slow = slow.next; fast = fast.next.next
+        prev = None; cur = slow
+        while cur: nxt = cur.next; cur.next = prev; prev = cur; cur = nxt
+        p, q = head, prev
+        while q:
+            p.next, p = q, p.next
+            q.next, q = p, q.next
