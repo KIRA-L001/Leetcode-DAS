7a152c266eb773ca32e8c75ae8c93e04342f6e51
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-08-19 11:48:54 +0530

    feat: add solution #139

---
+++ 0139-word-break.py
@@ -0,0 +1,8 @@
+from typing import List
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        words = set(wordDict); dp = [True] + [False]*len(s)
+        for i in range(1, len(s)+1):
+            for j in range(i):
+                if dp[j] and s[j:i] in words: dp[i] = True; break
+        return dp[-1]
