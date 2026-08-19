d753808da256e7b435d0563b9d1a03bb7edb16b9
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-08-19 11:48:56 +0530

    feat: add solution #113

---
+++ 0113-path-sum-ii.py
@@ -0,0 +1,15 @@
+from typing import List, Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val; self.left = left; self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        res = []
+        def dfs(n, s, path):
+            if not n: return
+            path.append(n.val)
+            if not n.left and not n.right and s == n.val: res.append(path[:])
+            dfs(n.left, s - n.val, path); dfs(n.right, s - n.val, path)
+            path.pop()
+        dfs(root, targetSum, [])
+        return res
