6eee247ebdb1615b61a2a2e730a8571d17353ed2
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-08-19 11:48:51 +0530

    feat: add solution #221

---
+++ 0221-maximal-square.py
@@ -0,0 +1,15 @@
+from typing import List
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        if not matrix: return 0
+        m, n = len(matrix), len(matrix[0]); dp = [0]*(n+1); best = 0
+        for i in range(m):
+            prev = 0
+            for j in range(1, n+1):
+                tmp = dp[j]
+                if matrix[i][j-1] == '1':
+                    dp[j] = min(dp[j], dp[j-1], prev) + 1
+                    best = max(best, dp[j])
+                else: dp[j] = 0
+                prev = tmp
+        return best * best
