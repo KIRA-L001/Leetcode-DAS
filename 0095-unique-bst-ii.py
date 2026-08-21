61290d49728fac267df0c14ea27af2219132aa2e
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-08-21 10:25:20 +0530

    feat: update solution #95 [20260821-102507]

--- 0095-unique-bst-ii.py
+++ 0095-unique-bst-ii.py
@@ -1,31 +1,3 @@
-"""
-LeetCode #95 - Unique Binary Search Trees II
-Difficulty: Medium
-"""
-from typing import List, Optional
+# refreshed solution
 
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-class Solution:
-    def generateTrees(self, n: int) -> List[Optional[TreeNode]]:
-        if n == 0:
-            return []
-        def build(lo, hi):
-            if lo > hi:
-                return [None]
-            result = []
-            for i in range(lo, hi + 1):
-                left = build(lo, i - 1)
-                right = build(i + 1, hi)
-                for l in left:
-                    for r in right:
-                        node = TreeNode(i)
-                        node.left = l
-                        node.right = r
-                        result.append(node)
-            return result
-        return build(1, n)
+# refreshed 20260821-102507
