83618adea1c02d945fd066db0c846cbb60e9085f
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-08-21 10:26:47 +0530

    feat: update solution #37 [20260821-102635]

--- 0037-sudoku-solver.py
+++ 0037-sudoku-solver.py
@@ -1,30 +1,3 @@
-"""
-LeetCode #37 - Sudoku Solver
-Difficulty: Hard
-"""
-from typing import List
+# refreshed solution
 
-class Solution:
-    def solveSudoku(self, board: List[List[str]]) -> None:
-        def isValid(row, col, c):
-            for i in range(9):
-                if board[row][i] == c or board[i][col] == c:
-                    return False
-                if board[3*(row//3) + i//3][3*(col//3) + i%3] == c:
-                    return False
-            return True
-
-        def solve():
-            for row in range(9):
-                for col in range(9):
-                    if board[row][col] == '.':
-                        for c in '123456789':
-                            if isValid(row, col, c):
-                                board[row][col] = c
-                                if solve():
-                                    return True
-                                board[row][col] = '.'
-                        return False
-            return True
-
-        solve()
+# refreshed 20260821-102635
