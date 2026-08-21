f5ecef007166d2697c9254c172a27395af8e303d
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-08-21 10:25:08 +0530

    feat: update solution #112 [20260821-102507]

--- 0112-path-sum.py
+++ 0112-path-sum.py
@@ -7,3 +7,5 @@ class Solution:
         if not root: return False
         if not root.left and not root.right: return root.val == targetSum
         return self.hasPathSum(root.left, targetSum - root.val) or self.hasPathSum(root.right, targetSum - root.val)
+
+# refreshed 20260821-102507
