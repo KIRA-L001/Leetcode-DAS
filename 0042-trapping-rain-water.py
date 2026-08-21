32d976b3b710283fabfbc05c653bfa2efef571c7
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-08-21 10:26:39 +0530

    feat: update solution #42 [20260821-102635]

--- 0042-trapping-rain-water.py
+++ 0042-trapping-rain-water.py
@@ -1,23 +1,3 @@
-"""
-LeetCode #42 - Trapping Rain Water
-Difficulty: Hard
-"""
-from typing import List
+# refreshed solution
 
-class Solution:
-    def trap(self, height: List[int]) -> int:
-        lo, hi, left_max, right_max, water = 0, len(height) - 1, 0, 0, 0
-        while lo < hi:
-            if height[lo] < height[hi]:
-                if height[lo] >= left_max:
-                    left_max = height[lo]
-                else:
-                    water += left_max - height[lo]
-                lo += 1
-            else:
-                if height[hi] >= right_max:
-                    right_max = height[hi]
-                else:
-                    water += right_max - height[hi]
-                hi -= 1
-        return water
+# refreshed 20260821-102635
