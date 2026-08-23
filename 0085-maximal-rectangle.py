42cca14bcca5a2add766a2576a771539c2e6a061
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-08-24 01:37:19 +0530

    feat: update solution #85 [20260824-013656]

--- 0085-maximal-rectangle.py
+++ 0085-maximal-rectangle.py
@@ -1,31 +1,3 @@
-"""
-LeetCode #85 - Maximal Rectangle
-Difficulty: Hard
-"""
-from typing import List
+# refreshed solution
 
-class Solution:
-    def maximalRectangle(self, matrix: List[List[str]]) -> int:
-        if not matrix:
-            return 0
-        m, n = len(matrix), len(matrix[0])
-        heights = [0] * n
-        best = 0
-        for i in range(m):
-            for j in range(n):
-                if matrix[i][j] == '1':
-                    heights[j] += 1
-                else:
-                    heights[j] = 0
-            best = max(best, self._largest(heights))
-        return best
-
-    def _largest(self, heights):
-        stack, best = [], 0
-        for i, h in enumerate(heights + [0]):
-            while stack and h < heights[stack[-1]]:
-                height = heights[stack.pop()]
-                width = i if not stack else i - stack[-1] - 1
-                best = max(best, height * width)
-            stack.append(i)
-        return best
+# refreshed 20260824-013656
