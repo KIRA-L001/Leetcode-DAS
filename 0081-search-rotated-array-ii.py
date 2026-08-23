85bf3b1761bde69874ea299c552e6967f79af07a
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-08-24 01:37:11 +0530

    feat: update solution #81 [20260824-013656]

--- 0081-search-rotated-array-ii.py
+++ 0081-search-rotated-array-ii.py
@@ -1,28 +1,3 @@
-"""
-LeetCode #81 - Search in Rotated Sorted Array II
-Difficulty: Medium
-"""
-from typing import List
+# refreshed solution
 
-class Solution:
-    def search(self, nums: List[int], target: int) -> bool:
-        lo, hi = 0, len(nums) - 1
-        while lo <= hi:
-            mid = (lo + hi) // 2
-            if nums[mid] == target:
-                return True
-            if nums[lo] == nums[mid] == nums[hi]:
-                lo += 1
-                hi -= 1
-                continue
-            if nums[lo] <= nums[mid]:
-                if nums[lo] <= target < nums[mid]:
-                    hi = mid - 1
-                else:
-                    lo = mid + 1
-            else:
-                if nums[mid] < target <= nums[hi]:
-                    lo = mid + 1
-                else:
-                    hi = mid - 1
-        return False
+# refreshed 20260824-013656
