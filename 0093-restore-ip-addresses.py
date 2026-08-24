4ab579f2f9ef39ba621cc921ae7134fb35554c32
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-08-24 11:12:47 +0530

    feat: update solution #93 [20260824-111237]

--- 0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses.py
@@ -1,21 +1,3 @@
-"""
-LeetCode #93 - Restore IP Addresses
-Difficulty: Medium
-"""
-from typing import List
+# refreshed solution
 
-class Solution:
-    def restoreIpAddresses(self, s: str) -> List[str]:
-        result = []
-        def backtrack(start, path):
-            if len(path) == 4:
-                if start == len(s):
-                    result.append(".".join(path))
-                return
-            for i in range(start + 1, min(start + 4, len(s)) + 1):
-                segment = s[start:i]
-                if (len(segment) > 1 and segment[0] == '0') or int(segment) > 255:
-                    continue
-                backtrack(i, path + [segment])
-        backtrack(0, [])
-        return result
+# refreshed 20260824-111237
