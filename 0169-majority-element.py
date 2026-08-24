a1815dacc82cf59f029e491dff3bfac8b230a693
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-08-24 21:00:15 +0530

    chore: 9pm auto-commit

--- 0169-majority-element.py
+++ 0169-majority-element.py
@@ -1,15 +1,10 @@
-"""
-LeetCode #169 - Majority Element
-Difficulty: Easy
-"""
 from typing import List
-
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count = 0
-        candidate = None
-        for num in nums:
-            if count == 0:
-                candidate = num
-            count += 1 if num == candidate else -1
-        return candidate
+        cand = cnt = 0
+        for n in nums:
+            if cnt == 0: cand = n
+            cnt += 1 if n == cand else -1
+        return cand
+
+# refreshed 20260824-013502
