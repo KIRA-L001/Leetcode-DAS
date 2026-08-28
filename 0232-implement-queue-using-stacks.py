0c9cdffaf3a489c0c80f3272b0d1d6a8e1737280
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-08-28 14:23:42 +0530

    feat: update solution #232 [20260828-142333]

--- 0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks.py
@@ -1,27 +1,8 @@
-"""
-LeetCode #232 - Implement Queue using Stacks
-Difficulty: Easy
-"""
 class MyQueue:
-    def __init__(self):
-        self.stack_in = []
-        self.stack_out = []
+    def __init__(self): self.s = []
+    def push(self, x: int) -> None: self.s.append(x)
+    def pop(self) -> int: return self.s.pop(0)
+    def peek(self) -> int: return self.s[0]
+    def empty(self) -> bool: return not self.s
 
-    def push(self, x):
-        self.stack_in.append(x)
-
-    def pop(self):
-        self._transfer()
-        return self.stack_out.pop()
-
-    def peek(self):
-        self._transfer()
-        return self.stack_out[-1]
-
-    def empty(self):
-        return not self.stack_in and not self.stack_out
-
-    def _transfer(self):
-        if not self.stack_out:
-            while self.stack_in:
-                self.stack_out.append(self.stack_in.pop())
+# refreshed 20260828-142333
