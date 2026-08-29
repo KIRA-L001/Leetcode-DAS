a47c30c5f289b25e82b432ed0d0897b1d6a4c660
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-08-29 13:20:21 +0530

    feat: update solution #79 [20260829-132014]

--- 0079-word-search.py
+++ 0079-word-search.py
@@ -1,24 +1,3 @@
-"""
-LeetCode #79 - Word Search
-Difficulty: Medium
-"""
-from typing import List
+# refreshed solution
 
-class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
-        rows, cols = len(board), len(board[0])
-        def dfs(r, c, i):
-            if i == len(word):
-                return True
-            if r < 0 or r >= rows or c < 0 or c >= cols or board[r][c] != word[i]:
-                return False
-            tmp = board[r][c]
-            board[r][c] = '#'
-            found = dfs(r+1, c, i+1) or dfs(r-1, c, i+1) or dfs(r, c+1, i+1) or dfs(r, c-1, i+1)
-            board[r][c] = tmp
-            return found
-        for r in range(rows):
-            for c in range(cols):
-                if dfs(r, c, 0):
-                    return True
-        return False
+# refreshed 20260829-132014
