d0bc965cacf0ff2c7b17859a89c49ac558596050
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-08-30 23:08:57 +0530

    feat: update solution #344 [20260830-230832]

--- 0344-reverse-string.py
+++ 0344-reverse-string.py
@@ -1,21 +1,5 @@
-"""
-344. Reverse String
-Write a function that reverses a string. Input is a character array, modify in-place.
+class Solution:
+    def reverseString(self, s: list) -> None:
+        s.reverse()
 
-Approach: Two pointers swap from ends towards center.
-Time: O(n/2)  Space: O(1)
-"""
-from typing import List
-
-def reverseString(s: List[str]) -> None:
-    l, r = 0, len(s) - 1
-    while l < r:
-        s[l], s[r] = s[r], s[l]
-        l += 1
-        r -= 1
-
-if __name__ == "__main__":
-    s = list("hello")
-    reverseString(s)
-    assert s == list("olleh")
-    print("0344 OK")
+# refreshed 20260830-230832
