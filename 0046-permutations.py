38c0c72e0c42d95f65a4f351fad9a52317df70cb
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-08-31 13:29:52 +0530

    feat: update solution #46 [20260831-130836]

--- 0046-permutations.py
+++ 0046-permutations.py
@@ -1,21 +1,3 @@
-"""
-LeetCode #46 - Permutations
-Difficulty: Medium
-"""
-from typing import List
+# refreshed solution
 
-class Solution:
-    def permute(self, nums: List[int]) -> List[List[int]]:
-        result = []
-        def backtrack(path):
-            if len(path) == len(nums):
-                result.append(path[:])
-                return
-            for n in nums:
-                if n in path:
-                    continue
-                path.append(n)
-                backtrack(path)
-                path.pop()
-        backtrack([])
-        return result
+# refreshed 20260831-130836
