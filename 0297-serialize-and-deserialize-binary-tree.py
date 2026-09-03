528d20ee0bd15ea8d86463f2f53b5d980f6adb36
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-09-03 10:00:24 +0530

    feat: update solution #297 [20260903-100015]

--- 0297-serialize-and-deserialize-binary-tree.py
+++ 0297-serialize-and-deserialize-binary-tree.py
@@ -1,36 +1,3 @@
-"""
-LeetCode #297 - Serialize and Deserialize Binary Tree
-Difficulty: Hard
-"""
-from typing import Optional
+# refreshed solution
 
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-class Codec:
-    def serialize(self, root: Optional[TreeNode]) -> str:
-        def dfs(node):
-            if not node:
-                vals.append("None")
-                return
-            vals.append(str(node.val))
-            dfs(node.left)
-            dfs(node.right)
-        vals = []
-        dfs(root)
-        return ",".join(vals)
-
-    def deserialize(self, data: str) -> Optional[TreeNode]:
-        vals = iter(data.split(","))
-        def dfs():
-            val = next(vals)
-            if val == "None":
-                return None
-            node = TreeNode(int(val))
-            node.left = dfs()
-            node.right = dfs()
-            return node
-        return dfs()
+# refreshed 20260903-100015
