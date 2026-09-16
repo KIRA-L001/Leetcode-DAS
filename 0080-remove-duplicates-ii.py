a31d1ad86483f1d3149a7f59b56c0b020fe00baa
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-09-16 10:01:05 +0530

    feat: update solution #80 [20260916-100057]

--- 0080-remove-duplicates-ii.py
+++ 0080-remove-duplicates-ii.py
@@ -1,16 +1,3 @@
-"""
-LeetCode #80 - Remove Duplicates from Sorted Array II
-Difficulty: Medium
-"""
-from typing import List
+# refreshed solution
 
-class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        if len(nums) <= 2:
-            return len(nums)
-        i = 2
-        for j in range(2, len(nums)):
-            if nums[j] != nums[i - 2]:
-                nums[i] = nums[j]
-                i += 1
-        return i
+# refreshed 20260916-100057
