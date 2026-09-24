36f2a209db2f0de21bd0cd0c3a1de616e9bf8685
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-09-24 10:00:52 +0530

    feat: update solution #69 [20260924-100049]

--- 0069-sqrtx.py
+++ 0069-sqrtx.py
@@ -1,16 +1,3 @@
-"""
-LeetCode #69 - Sqrt(x)
-Difficulty: Easy
-"""
-class Solution:
-    def mySqrt(self, x: int) -> int:
-        lo, hi = 0, x
-        while lo <= hi:
-            mid = (lo + hi) // 2
-            if mid * mid <= x < (mid + 1) * (mid + 1):
-                return mid
-            elif mid * mid > x:
-                hi = mid - 1
-            else:
-                lo = mid + 1
-        return lo
+# refreshed solution
+
+# refreshed 20260924-100049
