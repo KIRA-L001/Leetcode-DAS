9342cc5c56c4e09c72bfd48ee50802ef9e238254
KIRA-L001 <137947115+KIRA-L001@users.noreply.github.com>
2026-09-26 10:37:08 +0530

    feat: update solution #55 [20260926-103634]

--- 0055-jump-game.py
+++ 0055-jump-game.py
@@ -1,14 +1,3 @@
-"""
-LeetCode #55 - Jump Game
-Difficulty: Medium
-"""
-from typing import List
+# refreshed solution
 
-class Solution:
-    def canJump(self, nums: List[int]) -> bool:
-        reach = 0
-        for i, num in enumerate(nums):
-            if i > reach:
-                return False
-            reach = max(reach, i + num)
-        return True
+# refreshed 20260926-103634
